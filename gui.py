6e656444b8577726e8ff4f883004b76c83d5bbce
Vijay-Vengatesh <81487426+Vijay-Vengatesh@users.noreply.github.com>
2024-11-12 21:13:47 +0530

    edit button added to user interface

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import FreeSimpleGUI as fsg
 label = fsg.Text("Type in a todo")
 input_box = fsg.InputText(tooltip="Enter todo", key='todos')
 add_button = fsg.Button("Add")
+list_box = fsg.Listbox(values=functions.get_todos(), key='todoslist',
+                       enable_events=True, size=[47, 10])
+edit_button = fsg.Button("Edit")
 
 window = fsg.Window('My Todo App',
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -19,6 +22,18 @@ while True:
             new_todos = values['todos'] + '\n'
             todos.append(new_todos)
             functions.write_todos(todos_arg=todos)
+            window['todoslist'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todoslist'][0]
+            ntodos = values['todos'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos.__setitem__(index, ntodos)
+            functions.write_todos(todos)
+            window['todoslist'].update(values=todos)
+        case 'todoslist':
+            window['todos'].update(value=values['todoslist'][0])
         case fsg.WIN_CLOSED:
             break
 
