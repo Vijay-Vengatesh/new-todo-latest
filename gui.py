6d69df5225860923622686a8355ad2c0687939c0
Vijay-Vengatesh <81487426+Vijay-Vengatesh@users.noreply.github.com>
2024-11-15 23:53:18 +0530

    Implemented complete and exit button

--- gui.py
+++ gui.py
@@ -7,9 +7,13 @@ add_button = fsg.Button("Add")
 list_box = fsg.Listbox(values=functions.get_todos(), key='todoslist',
                        enable_events=True, size=[47, 10])
 edit_button = fsg.Button("Edit")
+complete_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 
 window = fsg.Window('My Todo App',
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button, exit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -32,8 +36,17 @@ while True:
             todos.__setitem__(index, ntodos)
             functions.write_todos(todos)
             window['todoslist'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todoslist'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todoslist'].update(values=todos)
+            window['todos'].update(value='')
         case 'todoslist':
             window['todos'].update(value=values['todoslist'][0])
+        case 'Exit':
+            quit()
         case fsg.WIN_CLOSED:
             break
 
